5620a4b3a140f3136d8b90361ad1b3463ab16592
Matteo Cristino <102997993+matteo-cristino@users.noreply.github.com>
2022-11-12 12:20:08 +0100

    Update javascript.mk

--- build/javascript.mk
+++ build/javascript.mk
@@ -54,7 +54,7 @@ javascript-wasm: ${BUILDS}
 	@cp -v src/zenroom.wasm build/wasm/
 
 javascript-npm: cflags  += -O3
-javascript-npm: cflags  += -DARCH_WASM -D'ARCH=\"WASM\"'
+javascript-npm: cflags  += -DARCH_WASM -D'ARCH=\"WASM\"' -msse -msse2 -msimd128
 javascript-npm: ldflags += -s \
 	-s MODULARIZE=1 \
 	-s SINGLE_FILE=1 \
